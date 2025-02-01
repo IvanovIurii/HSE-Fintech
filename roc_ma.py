26caaf5e8185e05dcaf1b2ae2dc3fcbad0dbefc5
Iurii Ivanov <yuran2016@gmail.com>
2025-02-01 15:17:42 +0100

    roc indicator buy/sell signals

--- roc_ma.py
+++ roc_ma.py
@@ -33,34 +33,121 @@ def parseData(file_name: str):
     return data
 
 
-data = parseData('./resources/AAPL.csv')
-data['roc'] = ROCIndicator(data['close'], window=14).roc()
+def get_roc_indicator(data, roc_window: int):
+    data['roc'] = ROCIndicator(data['close'], window=roc_window).roc()
 
-data = data.loc['2022-01-01':'2024-12-31']
+    return data
 
-data['signal'] = 0
-data['signal'][data['roc'] > 0] = 1
-data['signal'][data['roc'] < 0] = -1
 
-data['group'] = (data['signal'] != data['signal'].shift(1)).cumsum()
-grouped = data.groupby(['group', 'signal']).agg(
-    timestamp=('date', 'first'),
-    count=('signal', 'size')
-).reset_index()
+def prepare_buy_sell_signals(data):
+    data['signal'] = 0
+    data['signal'][data['roc'] > 0] = 1
+    data['signal'][data['roc'] < 0] = -1
 
-# On signal wait:
-# buy when 2 bars after the signal;
-# sell after 2 bars after the signal.
-grouped['action'] = pd.NA
-grouped.loc[(grouped['count'] > 2) & (grouped['signal'] == 1), 'action'] = 'buy'
-grouped.loc[(grouped['count'] > 2) & (grouped['signal'] == -1), 'action'] = 'sell'
+    return data
 
-# todo: rename
-list_of_dicts = grouped.to_dict(orient='records')
-dict_df = pd.DataFrame(list_of_dicts)
-data = pd.merge(data, dict_df[['timestamp', 'action']], on='timestamp', how='left')
 
-data['action'] = data['action'].fillna(-1)
+def prepare_buy_sell_actions(data):
+    data['group'] = (data['signal'] != data['signal'].shift(1)).cumsum()
+
+    grouped_data = data.groupby(['group', 'signal']).agg(
+        timestamp=('date', 'first'),
+        count=('signal', 'size')
+    ).reset_index()
+
+    # On signal wait:
+    # buy when 2 bars after the signal;
+    # sell after 2 bars after the signal.
+    grouped_data['action'] = pd.NA
+    grouped_data.loc[(grouped_data['count'] > 2) & (grouped_data['signal'] == 1), 'action'] = 'buy'
+    grouped_data.loc[(grouped_data['count'] > 2) & (grouped_data['signal'] == -1), 'action'] = 'sell'
+
+    grouped_data_to_merge = pd.DataFrame(grouped_data.to_dict(orient='records'))
+
+    data = pd.merge(
+        data,
+        grouped_data_to_merge[['timestamp', 'action']],
+        on='timestamp',
+        how='left'
+    )
+    data['action'] = data['action'].fillna(-1)
+
+    return data
+
+
+def show_charts(data):
+    buy_signals = data[data['action'] == 'buy']
+    sell_signals = data[data['action'] == 'sell']
+
+    fig = make_subplots(
+        rows=2, cols=1,
+        shared_xaxes=True,
+        vertical_spacing=0.15,
+        subplot_titles=("Price chart with Buy/Sell Signals", "Rate of Change (ROC) Indicator"),
+        row_heights=[0.7, 0.3]
+    )
+
+    fig.add_trace(go.Candlestick(
+        x=data['date'],
+        open=data['open'],
+        high=data['high'],
+        low=data['low'],
+        close=data['close'],
+        name='Close Price'
+    ), row=1, col=1)
+
+    # todo: extract to single method traces below
+    fig.add_trace(go.Scatter(
+        x=buy_signals['date'],
+        y=buy_signals['low'] - 10,  # Place buy marker slightly below the low of the candle for better observability
+        mode='markers',
+        marker=dict(symbol='triangle-up', color='green', size=15),
+        name='Buy Signal'
+    ), row=1, col=1)
+
+    fig.add_trace(go.Scatter(
+        x=sell_signals['date'],
+        y=sell_signals['low'] - 10,  # Place buy marker slightly below the low of the candle
+        mode='markers',
+        marker=dict(symbol='triangle-up', color='red', size=15),
+        name='Sell Signal'
+    ), row=1, col=1)
+
+    fig.add_trace(go.Scatter(
+        x=data['date'],
+        y=data['roc'],
+        mode='lines',
+        name='ROC',
+        line=dict(color='black', width=2)
+    ), row=2, col=1)
+
+    fig.update_layout(
+        title="Stock chart - ROC based strategy with signals",
+        xaxis_title="Date",
+        yaxis_title="Price",
+        xaxis_rangeslider_visible=False,
+        height=800,
+        showlegend=True
+    )
+
+    fig.update_yaxes(title_text="Price", row=1, col=1)
+    fig.update_yaxes(title_text="ROC (%)", row=2, col=1)
+
+    fig.show()
+
+
+def main(stock_data_file_name: str, roc_window: int, start_date: str, end_date: str):
+    data = parseData(stock_data_file_name)
+
+    data = get_roc_indicator(data, roc_window)
+    data = data.loc[start_date:end_date]
+    data = prepare_buy_sell_signals(data)
+    data = prepare_buy_sell_actions(data)
+
+    show_charts(data)
+
+
+main('./resources/AAPL.csv', 14, start_date='2022-01-01', end_date='2024-12-31')
 
 # todo: move to its own method calculate profit
 # buy_prices = []
@@ -79,66 +166,6 @@ data['action'] = data['action'].fillna(-1)
 #
 # result = sum(buy_prices) - sum(sell_prices)
 
-buy_signals = data[data['action'] == 'buy']
-sell_signals = data[data['action'] == 'sell']
 
-fig = make_subplots(
-    rows=2, cols=1,
-    shared_xaxes=True,
-    vertical_spacing=0.15,
-    subplot_titles=("Price chart with Buy/Sell Signals", "Rate of Change (ROC) Indicator"),
-    row_heights=[0.7, 0.3]
-)
-
-fig.add_trace(go.Candlestick(
-    x=data['date'],
-    open=data['open'],
-    high=data['high'],
-    low=data['low'],
-    close=data['close'],
-    name='Close Price'
-), row=1, col=1)
-
-# todo: extract to single method traces below
-fig.add_trace(go.Scatter(
-    x=buy_signals['date'],
-    y=buy_signals['low'] - 10,  # Place buy marker slightly below the low of the candle for better observability
-    mode='markers',
-    marker=dict(symbol='triangle-up', color='green', size=15),
-    name='Buy Signal'
-), row=1, col=1)
-
-fig.add_trace(go.Scatter(
-    x=sell_signals['date'],
-    y=sell_signals['low'] - 10,  # Place buy marker slightly below the low of the candle
-    mode='markers',
-    marker=dict(symbol='triangle-up', color='red', size=15),
-    name='Sell Signal'
-), row=1, col=1)
-
-fig.add_trace(go.Scatter(
-    x=data['date'],
-    y=data['roc'],
-    mode='lines',
-    name='ROC',
-    line=dict(color='black', width=2)
-), row=2, col=1)
-
-fig.update_layout(
-    title="Stock chart - ROC based strategy with signals",
-    xaxis_title="Date",
-    yaxis_title="Price",
-    xaxis_rangeslider_visible=False,
-    height=800,
-    showlegend=True
-)
-
-fig.update_yaxes(title_text="Price", row=1, col=1)
-fig.update_yaxes(title_text="ROC (%)", row=2, col=1)
-
-fig.show()
-
-# TODO: refactor
 # TODO: calculate returns
-
 # TODO: when to start and how much to invest?
